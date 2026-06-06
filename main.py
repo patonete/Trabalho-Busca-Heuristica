4ff7962f104f23892c68bdc56ab480f578ac4af7
patonete <patonete2005@hotmail.com>
2026-06-06 14:45:57 -0300

    Adiciona exploracao inicial de vizinhos

--- main.py
+++ main.py
@@ -12,8 +12,22 @@ def heuristica(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 def a_star(mapa, inicio, objetivo):
-    print("Executando busca...")
-    return [inicio, objetivo]
+
+    atual = inicio
+
+    x, y = atual
+
+    vizinhos = [
+        (x + 1, y),
+        (x - 1, y),
+        (x, y + 1),
+        (x, y - 1)
+    ]
+
+    print("Vizinhos encontrados:")
+    print(vizinhos)
+
+    return [inicio]
 
 caminho = a_star(mapa, inicio, objetivo)
 
